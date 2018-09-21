06bc31061d693b214c780e2dcaaf6d9dcc80529b
Shaun <shaundavidrussell@yahoo.com>
2018-09-21 13:52:34 -0500

    Group Add Remove Btns

--- src/components/AddRemoveFormGroupBtns.js
+++ src/components/AddRemoveFormGroupBtns.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Button } from 'semantic-ui-react'
 
 export const AddRemoveFormGroupBtns = ({ onMinusClick, onPlusClick }) => (
   <div style={{cursor: "pointer", padding: "5px 0", display: "inline"}}>
-    <Icon
-      name="plus"
-      onClick={onPlusClick} >
-    </Icon>
-
-    <Icon
-      name="minus"
-      onClick={onMinusClick} >
-    </Icon>
+    <Button.Group icon>
+       <Button>
+         <Icon
+           name='plus'
+           onClick={onPlusClick}
+         />
+       </Button>
+       <Button>
+         <Icon name='minus'
+           name="minus"
+           onClick={onMinusClick}
+         />
+       </Button>
+     </Button.Group>
   </div>
 )
