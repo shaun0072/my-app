eab90fbb2defa725d712d57c4d31edca623b24c6
Shaun <shaundavidrussell@yahoo.com>
2018-12-15 07:39:09 -0600

    it been a while

--- src/server/php/add_product.php
+++ src/server/php/add_product.php
@@ -15,150 +15,150 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST') {
 	//Set all array keys to variables with the same name
 	extract($_POST);
 
-	// $auto_generate = '';
-	//
-  // //Connect to tmfc_db and store result in variable
-	// $conn = connect_to_db();
-	//
-	// //ENTER PRODUCT INFO INTO PRODUCT TABLE
-	// $add_product_stmt = $conn->prepare("INSERT INTO products (
-	// 	product_id,
-	// 	product_name,
-	// 	model_number,
-	// 	category_id
-	// )
-	// VALUES (?,?,?,?)");
-	//
-	// //bind
-	// $add_product_stmt->bind_param('sssi', $auto_generate, $product_name, $model_number, $category_id);
-	//
-	// //execute
-	// $execute = $add_product_stmt->execute();
-	// if(!$execute) {
-	// 	echo htmlspecialchars($add_product_stmt->error);
-	// 	exit;
-	// }
-	//
-	// $last_product_id = $conn->insert_id;
-	//
-	// if($description) {
-	// 	$add_tags_stmt = $conn->prepare("INSERT INTO tags (
-	// 		tags_id,
-	// 		product_id,
-	// 		tags
-	// 	)
-	// 	VALUES (?,?,?)");
-	//
-	// 	//bind
-	// 	$add_tags_stmt->bind_param('sis', $auto_generate, $last_product_id, $description);
-	//
-	// 	//execute
-	// 	$execute = $add_tags_stmt->execute();
-	// 	//handle errors
-	// 	if(!$execute) {
-	// 		echo htmlspecialchars($add_tags_stmt->error);
-	// 		exit;
-	// 	}
-	// }
-	//
-	// if($locations) {
-	// 	for($i=0;$i<count($locations);$i++) {
-	// 		$add_locations_stmt = $conn->prepare("INSERT INTO locations (
-	// 			location_id,
-	// 			product_id,
-	// 			location,
-	// 			current_qty,
-	// 			min_qty
-	// 		)
-	// 		VALUES (?,?,?,?,?)");
-	//
-	// 		//bind
-	// 		$add_locations_stmt->bind_param('sisii',
-	// 			$auto_generate,
-	// 			$last_product_id,
-	// 			$locations[$i]["location"],
-	// 			$locations[$i]["cur_qty"],
-	// 			$locations[$i]["min_qty"]
-	// 		);
-	//
-	// 		//execute
-	// 		$execute = $add_locations_stmt->execute();
-	// 		//handle errors
-	// 		if(!$execute) {
-	// 			echo htmlspecialchars($add_locations_stmt->error);
-	// 			exit;
-	// 		}
-	// 	}
-	// }
-	//
-	// if($vendors) {
-	// 	for($i=0;$i<count($vendors);$i++) {
-	// 		$vendor_id;
-	// 		//check if vendor exits in vendor table
-	// 		if ($add_vendors_stmt = $conn->prepare("SELECT vendor_id FROM vendors WHERE vendor_name=?")) {
-	//
-	// 	    /* bind parameters for markers */
-	// 	    $add_vendors_stmt->bind_param("s", $vendors[$i]["vendor"]);
-	//
-	// 	    /* execute query */
-	// 	    $add_vendors_stmt->execute();
-	//
-	// 	    /* bind result variables */
-	// 	    $add_vendors_stmt->bind_result($retrieved_vendor_id);
-	//
-	// 	    /* fetch value */
-	// 	    while($add_vendors_stmt->fetch()) {
-	// 				$vendor_id = $retrieved_vendor_id;
-	// 			}
-	//
-	// 			if(!isset($vendor_id)) {
-	// 				$add_vendor_stmt = $conn->prepare("INSERT INTO vendors (
-	// 					vendor_id,
-	// 					vendor_name
-	// 				)
-	// 				VALUES (?,?)");
-	//
-	// 				//bind
-	// 				$add_vendor_stmt->bind_param('ss', $auto_generate, $vendors[$i]["vendor"]);
-	//
-	// 				//execute
-	// 				$execute = $add_vendor_stmt->execute();
-	// 				//handle errors
-	// 				if(!$execute) {
-	// 					echo htmlspecialchars($add_vendor_stmt->error);
-	// 					exit;
-	// 				}
-	// 				$vendor_id = $conn->insert_id;
-	// 			}
-	// 		}
-	//
-	// 		$add_vendors_stmt = $conn->prepare("INSERT INTO product_vendor_info (
-	// 			product_vendor_info_id,
-	// 			vendor_id,
-	// 			product_id,
-	// 			item_number,
-	// 			vendor_link
-	// 		)
-	// 		VALUES (?,?,?,?,?)");
-	//
-	// 		//bind
-	// 		$add_vendors_stmt->bind_param('siiis',
-	// 			$auto_generate,
-	// 			$vendor_id,
-	// 			$last_product_id,
-	// 			$vendors[$i]["item_number"],
-	// 			$vendors[$i]["link_to_item"]
-	// 		);
-	//
-	// 		//execute
-	// 		$execute = $add_vendors_stmt->execute();
-	// 		//handle errors
-	// 		if(!$execute) {
-	// 			echo htmlspecialchars($add_vendors_stmt->error);
-	// 			exit;
-	// 		}
-	// 	}
-	// }
+	$auto_generate = '';
+
+  //Connect to tmfc_db and store result in variable
+	$conn = connect_to_db();
+
+	//ENTER PRODUCT INFO INTO PRODUCT TABLE
+	$add_product_stmt = $conn->prepare("INSERT INTO products (
+		product_id,
+		product_name,
+		model_number,
+		category_id
+	)
+	VALUES (?,?,?,?)");
+
+	//bind
+	$add_product_stmt->bind_param('sssi', $auto_generate, $product_name, $model_number, $category_id);
+
+	//execute
+	$execute = $add_product_stmt->execute();
+	if(!$execute) {
+		echo htmlspecialchars($add_product_stmt->error);
+		exit;
+	}
+
+	$last_product_id = $conn->insert_id;
+
+	if($description) {
+		$add_tags_stmt = $conn->prepare("INSERT INTO tags (
+			tags_id,
+			product_id,
+			tags
+		)
+		VALUES (?,?,?)");
+
+		//bind
+		$add_tags_stmt->bind_param('sis', $auto_generate, $last_product_id, $description);
+
+		//execute
+		$execute = $add_tags_stmt->execute();
+		//handle errors
+		if(!$execute) {
+			echo htmlspecialchars($add_tags_stmt->error);
+			exit;
+		}
+	}
+
+	if($locations) {
+		for($i=0;$i<count($locations);$i++) {
+			$add_locations_stmt = $conn->prepare("INSERT INTO locations (
+				location_id,
+				product_id,
+				location,
+				current_qty,
+				min_qty
+			)
+			VALUES (?,?,?,?,?)");
+
+			//bind
+			$add_locations_stmt->bind_param('sisii',
+				$auto_generate,
+				$last_product_id,
+				$locations[$i]["location"],
+				$locations[$i]["cur_qty"],
+				$locations[$i]["min_qty"]
+			);
+
+			//execute
+			$execute = $add_locations_stmt->execute();
+			//handle errors
+			if(!$execute) {
+				echo htmlspecialchars($add_locations_stmt->error);
+				exit;
+			}
+		}
+	}
+
+	if($vendors) {
+		for($i=0;$i<count($vendors);$i++) {
+			$vendor_id;
+			//check if vendor exits in vendor table
+			if ($add_vendors_stmt = $conn->prepare("SELECT vendor_id FROM vendors WHERE vendor_name=?")) {
+
+		    /* bind parameters for markers */
+		    $add_vendors_stmt->bind_param("s", $vendors[$i]["vendor"]);
+
+		    /* execute query */
+		    $add_vendors_stmt->execute();
+
+		    /* bind result variables */
+		    $add_vendors_stmt->bind_result($retrieved_vendor_id);
+
+		    /* fetch value */
+		    while($add_vendors_stmt->fetch()) {
+					$vendor_id = $retrieved_vendor_id;
+				}
+
+				if(!isset($vendor_id)) {
+					$add_vendor_stmt = $conn->prepare("INSERT INTO vendors (
+						vendor_id,
+						vendor_name
+					)
+					VALUES (?,?)");
+
+					//bind
+					$add_vendor_stmt->bind_param('ss', $auto_generate, $vendors[$i]["vendor"]);
+
+					//execute
+					$execute = $add_vendor_stmt->execute();
+					//handle errors
+					if(!$execute) {
+						echo htmlspecialchars($add_vendor_stmt->error);
+						exit;
+					}
+					$vendor_id = $conn->insert_id;
+				}
+			}
+
+			$add_vendors_stmt = $conn->prepare("INSERT INTO product_vendor_info (
+				product_vendor_info_id,
+				vendor_id,
+				product_id,
+				item_number,
+				vendor_link
+			)
+			VALUES (?,?,?,?,?)");
+
+			//bind
+			$add_vendors_stmt->bind_param('siiis',
+				$auto_generate,
+				$vendor_id,
+				$last_product_id,
+				$vendors[$i]["item_number"],
+				$vendors[$i]["link_to_item"]
+			);
+
+			//execute
+			$execute = $add_vendors_stmt->execute();
+			//handle errors
+			if(!$execute) {
+				echo htmlspecialchars($add_vendors_stmt->error);
+				exit;
+			}
+		}
+	}
 
 
 	echo json_encode($_POST);
